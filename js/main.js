ec6dfb0737afd0a0bdac0c3b17e76a8c7132b0bc
rlaecio <rlaecio@hotmail.com>
2023-03-16 20:37:57 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
+const itens = localStorage.getItem["itens"] || [];
+
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault();
+    nome = evento.target.elements['nome'];
+    quantidade = evento.target.elements['quantidade'];
+    
+    criaElemento(nome.value, quantidade.value);
+    nome.value = '';
+    quantidade.value = '';
+})
+
+
+function criaElemento(nome, quantidade) {
+    const novoItem = document.createElement('li');    
+    novoItem.classList.add("item");    
+    
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = quantidade;
+    novoItem.appendChild(numeroItem);
+    
+    novoItem.innerHTML += nome;
+
+    lista.appendChild(novoItem)
+    const itemAtual = {
+        "nome"       : nome,
+        "quantidade" : quantidade
+    }
+    itens.push(itemAtual);
+    localStorage.setItem("item", JSON.stringify(itens));
+}
